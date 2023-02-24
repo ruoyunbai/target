c983cd5fd1fab6a1c676b9b1541def76319789fc
gongwenxin <19376215@buaa.edu.cn>
2023-02-24 18:50:19 +0800

    auto_push

--- src/components/Serial.vue
+++ src/components/Serial.vue
@@ -8,7 +8,7 @@
             </el-select>
             <div>
             </div>
-            <el-tooltip class="box-item" effect="dark" content="若自定义波特率需要输入后单击右侧选项选中" placement="top-start">
+            <el-tooltip class="box-item" :enterable="false" effect="dark" content="若自定义波特率需要输入后单击右侧选项选中" placement="bottom">
 
 
                 <el-select v-model="baud" filterable allow-create :reserve-keyword="false" placeholder="输入或选择波特率">
@@ -26,9 +26,10 @@
                 </template>
                 <div class="mb-2 flex items-center text-sm">
                     <el-radio-group v-model="modeRec" class="ml-4">
-                        <el-radio label="asc" size="small">ASC II</el-radio>
+                        <el-radio label="ascii" size="small">ASC II</el-radio>
+                        <el-radio label="utf8" size="small">UTF8</el-radio>
                         <el-radio label="hex" size="small">HEX</el-radio>
-                        <el-radio label="bin" size="small">BIN</el-radio>
+                        <el-radio label="binary" size="small">BIN</el-radio>
                     </el-radio-group>
                 </div>
                 <el-divider />
@@ -46,11 +47,14 @@
                 </template>
                 <div class="mb-2 flex items-center text-sm">
                     <el-radio-group v-model="modeSend" class="ml-4">
-                        <el-radio label="asc" size="small">ASC II</el-radio>
+                        <el-radio label="ascii" size="small">ASC II</el-radio>
+                        <el-radio label="utf8" size="small">UTF8</el-radio>
                         <el-radio label="hex" size="small">HEX</el-radio>
-                        <el-radio label="bin" size="small">BIN</el-radio>
+                        <el-radio label="binary" size="small">BIN</el-radio>
                     </el-radio-group>
                 </div>
+                <el-divider/>
+                <el-checkbox v-model="ifAutoLine" label="自动换行" size="small" />
             </el-card>
         </el-col>
         <el-col :span="16">
@@ -60,7 +64,9 @@
                 <el-input style="white-space: pre-line;" v-model="content" :rows="10" type="textarea"
                     placeholder="接收到的内容" />
             </el-card>
-            <el-input v-model="sendStr" placeholder="输入发送的内容" class="input-with-select" @keyup.enter="handleSend()">
+            <el-tooltip class="box-item" effect="dark" content="回车或者点击图标发送" placement="bottom">
+                <el-input v-model="sendStr" placeholder="输入发送的内容(内容为空时将发送换行符)" class="input-with-select" @keyup.enter="handleSend()">
+          
 
                 <template #append>
                     <div>
@@ -68,6 +74,7 @@
                     </div>
                 </template>
             </el-input>
+        </el-tooltip>
         </el-col>
     </el-row>
 </template>
@@ -118,11 +125,19 @@ const bauds = [
 ]
 let sp: any
 const handleSend = () => {
-    // if(sendStr.value!="")
-    sp.write(sendStr.value + "\n")
-    sendStr.value = ""
-    // else
-    // sp.write("\n")
+    if(sendStr.value!=""){
+        sp.write(sendStr.value + "\n")
+        sendStr.value = ""
+    }
+    else
+    sp.write("\n")
+}
+//TODO不到八位则行首增加0
+function strToBinary(str: string) {
+    let list = str.split('');
+    return list.map((item: { charCodeAt: () => { (): any; new(): any; toString: { (arg0: number): any; new(): any; }; }; }) => {
+        return item.charCodeAt().toString(2);
+    }).join(' ');
 }
 const handleClick = () => {
     if (buttonType.value == "default") {
@@ -140,11 +155,17 @@ const handleClick = () => {
         sp.on('readable', () => {
             let str = sp.read()
             if (str != null) {
-                content.value += str.toString('utf8')
+                if(modeRec.value!="binary")
+                    content.value += str.toString(modeRec.value)
+                else{
+                    content.value +=  strToBinary(str.toString("utf8"))
+                }
+                if(ifAutoLine)content.value+="\n"
+
                 const textarea = document.getElementsByClassName('el-textarea__inner')[0];
                 textarea.scrollTop = textarea.scrollHeight;
 
-                console.log(str.toString('utf8'));
+                console.log(str);
             }// 使用read方法读取数据，可以指定读取字节数
         });
     } else {
